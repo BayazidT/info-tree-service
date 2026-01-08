f3c8b8c3a537db61068795f9b0129e6c5bb118f6
bayazidt <btnirob13@gmail.com>
2026-01-08 19:06:12 +0100

    main class name changed

--- src/main/java/com/trbtree/infotree/InfoTreeApplication.java
+++ src/main/java/com/trbtree/infotree/InfoTreeApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class RmsApplication {
+public class InfoTreeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RmsApplication.class, args);
+		SpringApplication.run(InfoTreeApplication.class, args);
 	}
 
 }
