217f115ce2647497dcb64712824ac8d0a49d90d8
bayazidt <btnirob13@gmail.com>
2026-01-21 23:16:59 +0100

    get civic service by id added

--- src/main/java/com/trbtree/infotree/modules/civic/repository/CivicServiceRepository.java
+++ src/main/java/com/trbtree/infotree/modules/civic/repository/CivicServiceRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CivicServiceRepository extends JpaRepository<CivicServiceEntity, Long> {
     @Query("""
@@ -14,4 +17,13 @@ public interface CivicServiceRepository extends JpaRepository<CivicServiceEntity
         JOIN FETCH cs.category
     """)
     Page<CivicServiceEntity> findAllWithJoins(Pageable pageable);
+
+    @Query("""
+        SELECT cs
+        FROM CivicServiceEntity cs
+        JOIN FETCH cs.city
+        JOIN FETCH cs.category
+        WHERE cs.id = :id
+    """)
+    Optional<CivicServiceEntity> findByIdWithJoins(@Param("id") Long id);
 }
